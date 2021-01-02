a0707d5bf31fae0ffc1ab3b9ca097bece4a654fa
Cullen S <cullensmi@gmail.com>
2021-01-01 20:14:10 -0500

    changed mongo url

--- myapp/express-locallibrary-tutorial/app.js
+++ myapp/express-locallibrary-tutorial/app.js
@@ -11,7 +11,6 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://pam:pam@cluster0.p9dfl.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
